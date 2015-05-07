ae961e2fed143cd7142884ad9f889cfcfdc3e34a
Artem V. Navrotskiy <bozaro@yandex.ru>
2015-05-07 11:35:38 +0300

    Add iron example

--- src/bin/octo_coordinator.rs
+++ src/bin/octo_coordinator.rs
@@ -1,13 +1,20 @@
 extern crate daemon;
+extern crate iron;
 
 use daemon::State;
 use daemon::Daemon;
 use daemon::DaemonRunner;
+use iron::prelude::*;
+use iron::status;
 use std::env;
 use std::fs::OpenOptions;
 use std::io::Error;
 use std::io::Write;
 use std::sync::mpsc::Receiver;
+
+fn hello_world(_: &mut Request) -> IronResult<Response> {
+	Ok(Response::with((status::Ok, "Hello World!")))
+}
 
 fn main() {
 	log("Example started.");
@@ -16,11 +23,25 @@ fn main() {
 	};
 	daemon.run(move |rx: Receiver<State>| {
 		log("Worker started.");
+		let mut web = None;
 		for signal in rx.iter() {
 			match signal {
-				State::Start => log("Worker: Start"),
-				State::Reload => log("Worker: Reload"),
-				State::Stop => log("Worker: Stop")
+				State::Start => {
+					log("Worker: Starting on 3000");
+					web = Some(Iron::new(hello_world).http("localhost:3000").unwrap());
+					log("Worker: Started");
+				},
+				State::Reload => {
+					log("Worker: Reload");
+				}
+				State::Stop => {
+					log("Worker: Stoping");
+					match web.take() {
+						Some(mut v) => {v.close().unwrap();}
+						None => {}
+					}
+					log("Worker: Stoped");
+				}
 			};
 		}
 		log("Worker finished.");
